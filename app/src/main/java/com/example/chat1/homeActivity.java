8054e96941547b298eda92c0770be71ae144e463
JosselynVela <deojosselyn@hotmail.com>
2020-07-24 03:05:15 -0500

    estado en linea o desconectado

--- app/src/main/java/com/example/chat1/homeActivity.java
+++ app/src/main/java/com/example/chat1/homeActivity.java
@@ -2,9 +2,13 @@ package com.example.chat1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,34 +16,247 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
+
+import com.example.chat1.adapters.PaginasAdapter;
+import com.example.chat1.pojos.Estado;
+import com.example.chat1.pojos.Users;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.zip.Inflater;
 
 public class homeActivity extends AppCompatActivity {
-    ImageView img_foto;
-    TextView tv_usuario,tv_mail;
+    //refrecnia al login para obtener informacion del usuario
+    FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    //hacemos la referencia para saber donde apunta FirebaseDatabase
+    // le llamaremos a nuestro registro Users que tendra un hijo que sera el id del usuario que ya hizo login
+    DatabaseReference ref_user=database.getReference("Users").child(user.getUid());
+
+    DatabaseReference ref_solicitud_count=database.getReference("Contador").child(user.getUid());
+    DatabaseReference ref_estado=database.getReference("Estado").child(user.getUid());
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        img_foto = findViewById(R.id.image_user);
-        tv_usuario= findViewById(R.id.tv_usuario);
-        tv_mail=findViewById(R.id.tv_mail);
+
+        //DECLARAR VALORES
+        ViewPager2 viewPager2 = findViewById(R.id.viewPager);
+        viewPager2.setAdapter(new PaginasAdapter(this));
+
+        final TabLayout tabLayout = findViewById(R.id.tabLayout);
+        //lo llamamos al tabloyout
+        TabLayoutMediator tabLayoutMediator=new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:{
+                        tab.setText("Users");
+                        tab.setIcon(R.drawable.ic_usuarios);
+                        break;
+                    }
+                    case 1:{
+                        tab.setText("Chats");
+                        tab.setIcon(R.drawable.ic_chats);
+                        break;
+                    }
+                    case 2:{
+                        tab.setText("Solicitudes");
+                        tab.setIcon(R.drawable.ic_solicitudes);
+                        final BadgeDrawable badgeDrawable = tab.getOrCreateBadge();
+                        badgeDrawable.setBackgroundColor(
+                                ContextCompat.getColor(getApplicationContext(),R.color.colorAccent)
+                        );
+
+
+                        ref_solicitud_count.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                //este condicional muestra el numero de solicitudes desde nuestra bbd
+                                if(dataSnapshot.exists()){
+                                    Integer val = dataSnapshot.getValue(Integer.class);
+                                    badgeDrawable.setVisible(true);
+                                    if (val.equals("0")){
+                                        badgeDrawable.setVisible(false);
+                                    }else {
+                                        badgeDrawable.setNumber(val);
+                                    }
+
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+                        break;
+                    }
+                    case 3:{
+                        tab.setText("Mis solicitudes");
+                        tab.setIcon(R.drawable.ic_mis_solicitudes);
+                        break;
+                    }
+
+                }
+
+            }
+        });
+
+        tabLayoutMediator.attach();
+
+        //me muestra el numero de solicitudes que recibo
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                BadgeDrawable badgeDrawable=tabLayout.getTabAt(position).getOrCreateBadge();
+                badgeDrawable.setVisible(false);
+
+                if (position==2){
+                    countacero();
+
+                }
+
+
+            }
+        });
+
+
         //llamando datos por medio de glide
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        Glide.with(this).load(user.getPhotoUrl()).into(img_foto);
-        tv_usuario.setText(user.getDisplayName());
-        tv_mail.setText(user.getEmail());
 
-        //mostrando datos del usuario
+        //creamos un metodo
 
+        userunico();
+
+
+
+
+    }
+
+    private void estadousuario(final String estado) {
+        ref_estado.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Estado est= new Estado(estado,"","");
+                ref_estado.setValue(est);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //mostraremos si el usuario esta activo o no
+        estadousuario("conectado");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        estadousuario("desconectado");
+        dameultimafecha();
+
+    }
+
+    private void dameultimafecha() {
+        final Calendar c = Calendar.getInstance();
+        final SimpleDateFormat timeformat = new SimpleDateFormat("HH:mm");
+        final SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy");
+
+        ref_estado.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ref_estado.child("fecha").setValue(dateformat.format(c.getTime()));
+                ref_estado.child("hora").setValue(timeformat.format(c.getTime()));            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void countacero() {
+        ref_solicitud_count.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    ref_solicitud_count.setValue(0);
+                    Toast.makeText(homeActivity.this,"Count badge a 0", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    //hacemos refrencia la bbd para crear un registro unico despues del login
+
+    private void userunico() {
+
+        //se debe usar una referencia unica para no crear bucles
+        ref_user.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //debemos consultar
+                if (!dataSnapshot.exists()){
+                    Users uu = new Users(
+                      //ponemos la forma que queremos estructurar la informacion y en q orden de los datos declarados en Users
+                            //en este orden se vera en firebase
+                      user.getUid(),
+                      user.getDisplayName(),
+                      user.getEmail(),
+                      user.getPhotoUrl().toString(),
+                      "desconectado",
+                      "22/07/2020",
+                      "23:39",
+                      0,
+                      0);
+
+                    /*el ref_user toma todo el obeto de arriba y se lo pasa a la referencia*/
+                    ref_user.setValue(uu);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/example/chat1/pojos/Users.java
@@ -0,0 +1,104 @@
+package com.example.chat1.pojos;
+
+public class Users {
+
+    //estrucutra de informacion para firebase
+
+    private String id;
+    private String nombre;
+    private String email;
+    private String foto;
+    private String estado;
+    private String fecha;
+    private String hora;
+    private int solicitudes;
+    private int nuevomensaje;
+    //se debe crear un constructor vacio para q funcione con firebase
+    public Users() {
+    }
+
+    //creamos nuestro metodo
+    public Users(String id, String nombre, String email, String foto, String estado, String fecha, String hora, int solicitudes, int nuevomensaje) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.foto = foto;
+        this.estado = estado;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.solicitudes = solicitudes;
+        this.nuevomensaje = nuevomensaje;
+    }
+    //creamos los getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public int getSolicitudes() {
+        return solicitudes;
+    }
+
+    public void setSolicitudes(int solicitudes) {
+        this.solicitudes = solicitudes;
+    }
+
+    public int getNuevomensaje() {
+        return nuevomensaje;
+    }
+
+    public void setNuevomensaje(int nuevomensaje) {
+        this.nuevomensaje = nuevomensaje;
+    }
+}
